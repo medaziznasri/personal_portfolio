8ef6c003b5d6fdeff8e43c62a6252ee8bdcef806
med aziz nasri <aziznasri11082003@gmail.com>
2024-12-18 11:56:09 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/", // Adjust for root deployment
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        notFound: resolve(__dirname, "404.html"), // Include the 404.html
-      },
-    },
-  },
-});
+  base:"/personal_portfolio"
+})
